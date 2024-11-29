2986a40b74bc636f0ee8614fa663d1d9350cf3c3
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2024-11-28 21:49:43 -0300

    Processamento do formulário de inscrição

---
+++ back-end/processa_form.php
@@ -0,0 +1,13 @@
+<?php
+    //print_r($_POST);
+    $nome = $_REQUEST['nome'];
+    $idade = $_REQUEST['idade'];
+    
+    echo "<h1>Dados do Usuário</h1>";
+    echo "<hr>";
+    echo "<p>O usuário $nome tem $idade anos.</p>";   
+
+    echo '<a href="../index.html"><button>Home Page</button></a>';
+    echo '<button type="button" onclick="history.back()">Voltar</button>';
+    echo '<button onclick="window.location.href=\'https:\\\\www.udesc.br\git ' ">Vai para Udesc</button>';
+    ?>
