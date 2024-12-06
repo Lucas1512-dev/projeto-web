80e58ce98c58907af97bc9a68cacf8f4015cd995
Lucas de Oliveira <lucas.oliveira1512@edu.udesc.br>
2024-12-05 22:11:39 -0300

    Correções

--- back-end/processa_form.php
+++ back-end/processa_form.php
@@ -9,5 +9,5 @@
 
     echo '<a href="../index.html"><button>Home Page</button></a>';
     echo '<button type="button" onclick="history.back()">Voltar</button>';
-    echo '<button onclick="window.location.href=\'https:\\\\www.udesc.br\git ' ">Vai para Udesc</button>';
+    echo '<button onclick="window.location.href=\'https:\\\\www.udesc.br\'">Vai para Udesc</button>';
     ?>
